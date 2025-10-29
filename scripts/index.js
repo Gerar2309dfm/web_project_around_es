9452659d06ac7bd6b5eb9f138e5ef66ef40e8a52
gerardo fernandez <gerardodavidf@icloud.com>
2025-10-29 17:43:49 -0600

    Editar nombre y cerrar ventana emergente

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,41 @@ const initialCards = [
 initialCards.forEach(function(card) {
     console.log(card.name)
 });
+
+const editButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('#edit-popup');
+const closeButton = editPopup.querySelector('.popup__close');
+const editForm = document.querySelector('#edit-profile-form');
+const nameInput = editPopup.querySelector('.popup__input_type_name');
+const jobInput = editPopup.querySelector('.popup__input_type_description');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__description');
+
+function openModal(modal) {
+  modal.classList.add('popup_is-opened');
+}
+
+function closeModal(modal) {
+  modal.classList.remove('popup_is-opened');
+}
+
+function fillProfileForm() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closeModal(editPopup);
+}
+
+editButton.addEventListener('click', handleOpenEditModal);
+closeButton.addEventListener('click', () => closeModal(editPopup));
+editForm.addEventListener('submit', handleProfileFormSubmit);
